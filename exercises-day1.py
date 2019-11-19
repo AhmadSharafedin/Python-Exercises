e0813c1175c8f742fddb065081221521bae77b64
AhmadSharafedin <sharafedin.ahmad@gmail.com>
2019-11-19 16:47:02 +0200

    Finish Python day1 exercises

---
+++ exercises-day1.py
@@ -0,0 +1,22 @@
+print ("Hello")
+
+print ("\tHello")
+print ("\t \tHello")
+print ("OrangeAcademey\n" *20 )
+x = float (1)
+y =float (2.8)
+z =float ("3")
+w =float ("4.2")
+print (x,y,z,w,sep ='\n')
+
+"""
+num1 =  int(input ("Enter the num"))
+num2 =  int(input ("Enter the num "))
+print (num1 * num2)
+"""
+
+num1 = int(input ("Enter the Base :")) 
+num2 = int(input ("Enter the Height :"))
+
+print ("The area of Triangle is",(num1*num2)/2)
+
